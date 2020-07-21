a045de678adfb1ff435e69e99671db7180935919
Bolaji Oyeneye <bioyeneye@gmail.com>
2020-07-21 10:04:14 +0100

    added console app for sample

---
+++ src/EmailSender.Smtp.Core.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmailSender.Smtp.Core.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
